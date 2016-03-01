547ba02ca35d71ed019c79830a0fa8e9b7455c62
IzzNarVewalo <cyprian.manthey@cs.tum.edu>
2016-03-01 13:42:03 +0100

    Inventory

---
+++ Assets/Scripte/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public enum potType
+    {
+        Small,
+        Medium,
+        Big,
+    }
+    public potType type;
+    public Sprite pot;
+    private int hpRecovery;
+
+    void Start()
+    {
+        if (type == potType.Small)
+            hpRecovery = 10;
+        if (type == potType.Medium)
+            hpRecovery = 20;
+        if (type == potType.Big)
+            hpRecovery = 30;
+    }
+}
