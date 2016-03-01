e8deacbcb8e07a05a2244ba99ae9e271f9012a94
Adrian <AdrianLoher@gmx.de>
2016-03-01 10:44:50 +0100

    enemyscript anpassung
    enemy wartet jetzt

--- Assets/Scripte/Enemy.cs
+++ Assets/Scripte/Enemy.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public int enemySpeed = 0;
-
+    public int turnSpeed = 1;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        float amToMove = enemySpeed * Time.deltaTime;
+        float amToMove = enemySpeed * Time.deltaTime * turnSpeed;
         transform.Translate(Vector3.right * amToMove);
 	}
 
@@ -22,13 +22,17 @@ public class Enemy : MonoBehaviour {
         Debug.Log("Test: " + otherObject.gameObject.name);
         if (otherObject.gameObject.tag == "Grenze")
         {
+            turnSpeed = 0;
+            StartCoroutine(turn());
+            Debug.Log("Davor: " + enemySpeed);
             enemySpeed *= -1;
-            //StartCoroutine(turn());
+            Debug.Log("Danach: " + enemySpeed);
         }
     }
 
-    //IEnumerator turn()
-    //{
-    //    yield return new WaitForSeconds(10.5f);
-    //}
+    IEnumerator turn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        turnSpeed = 1;
+    }
 }
