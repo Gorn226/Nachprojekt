c6f9b527ce111ef975300c1022d89b16cefa0fea
Manuel Neuberger <Manuel.Neuberger@live.de>
2016-03-01 16:18:43 +0100

    Menü

---
+++ Assets/Scripte/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void starten(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void verlassen()
+    {
+        Application.Quit();
+    }
+
+}
