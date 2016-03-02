d79972e2194bdeb986832a5663b7984e60877962
Manuel Neuberger <Manuel.Neuberger@live.de>
2016-03-02 10:37:21 +0100

    Menu läuft wieder

---
+++ Assets/Scripte/ButtonScripte.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScripte : MonoBehaviour {
+
+    public void starten(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void verlassen()
+    {
+        Application.Quit();
+    }
+}
